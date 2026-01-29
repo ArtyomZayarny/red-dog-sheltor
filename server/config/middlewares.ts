c96aa292da45edea28b277a0133714fd852bbe46
ArtemZayarny <artyom.zayarny@gmail.com>
2026-01-29 22:24:35 +0200

    fix: configure CORS to allow Vercel frontend
    - Add FRONTEND_URL env variable support
    - Allow localhost for development

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,8 +1,21 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:3000',
+        env('FRONTEND_URL', 'https://red-dog-sheltor.vercel.app'),
+      ].filter(Boolean),
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
