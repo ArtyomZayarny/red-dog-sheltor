9c2d737bcefede6924071e188f0dbffbb3679387
ArtemZayarny <artyom.zayarny@gmail.com>
2026-01-29 22:31:50 +0200

    fix: handle trailing slash in FRONTEND_URL for CORS

--- server/config/middlewares.ts
+++ server/config/middlewares.ts
@@ -1,25 +1,30 @@
-export default ({ env }) => [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: [
-        'http://localhost:5173',
-        'http://localhost:5174',
-        'http://localhost:3000',
-        env('FRONTEND_URL', 'https://red-dog-sheltor.vercel.app'),
-      ].filter(Boolean),
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
+export default ({ env }) => {
+  const frontendUrl = env('FRONTEND_URL', 'https://red-dog-sheltor.vercel.app').replace(/\/$/, '');
+
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    {
+      name: 'strapi::cors',
+      config: {
+        origin: [
+          'http://localhost:5173',
+          'http://localhost:5174',
+          'http://localhost:3000',
+          frontendUrl,
+          'https://red-dog-sheltor.vercel.app',
+        ],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+        keepHeaderOnError: true,
+      },
     },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+};
